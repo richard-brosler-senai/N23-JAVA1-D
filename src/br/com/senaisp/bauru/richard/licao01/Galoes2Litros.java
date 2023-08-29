7061a38e15cbd04d4578d90448cae2067cb3c9e0
Richard Brosler <richard.brosler@sp.senai.br>
2023-08-28 22:11:10 -0300

    Conteúdo da aula do dia 28-08-2023 - Lição 01 Eclipse

---
+++ src/br/com/senaisp/bauru/richard/licao01/Galoes2Litros.java
@@ -0,0 +1,13 @@
+package br.com.senaisp.bauru.richard.licao01;
+
+public class Galoes2Litros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double galoes = 10.0;
+		double galoes2Litros = 3.785;
+		double litros = galoes * galoes2Litros;
+		System.out.println("Litros = " + litros);
+	}
+
+}
