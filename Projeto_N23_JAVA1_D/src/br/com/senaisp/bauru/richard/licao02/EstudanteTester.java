6b57d1f00865079a34232736480ae80f4b64bdad
Richard Brosler <richard.brosler@sp.senai.br>
2023-08-29 21:03:07 -0300

    Conteúdo da aula 29-03-2023 - versão 1

---
+++ Projeto_N23_JAVA1_D/src/br/com/senaisp/bauru/richard/licao02/EstudanteTester.java
@@ -0,0 +1,20 @@
+package br.com.senaisp.bauru.richard.licao02;
+
+public class EstudanteTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Estudante est = new Estudante();
+		
+		est.setNome("Zé da esquina");
+		est.setIdade(50);
+		est.setAltura(1.75);
+		est.setFumante(true);
+		
+		Estudante est2 = new Estudante("Joaquim", 60, 1.60, false);
+		
+		System.out.println("Nome: " + est.getNome());
+		System.out.println(est2.toString());
+	}
+
+}
